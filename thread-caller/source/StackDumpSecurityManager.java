8e4743d0a0101b471bffa8188fc287f2e9cf83ac
oleg pavliv <oleg.pavliv@gmail.com>
2011-09-30 15:36:41 +0200

    add checkRead

--- thread-caller/source/StackDumpSecurityManager.java
+++ thread-caller/source/StackDumpSecurityManager.java
@@ -19,12 +19,158 @@ public class StackDumpSecurityManager extends SecurityManager  {
 
     private PropertyTuple includeCreateThread = new PropertyTuple("includeCreateThread", null, false);
     private PropertyTuple excludeCreateThread = new PropertyTuple("excludeCreateThread", null, false);
+    private PropertyTuple includeReadFile = new PropertyTuple("includeReadFile", null, false);
+    private PropertyTuple excludeReadFile = new PropertyTuple("excludeReadFile", null, false);
     private PropertyTuple includeWriteFile = new PropertyTuple("includeWriteFile", null, false);
     private PropertyTuple excludeWriteFile = new PropertyTuple("excludeWriteFile", null, false);
 
     private int logLevel = 0;
     private String traceElementSeparator = ";; ";
 
+
+    public StackDumpSecurityManager() {
+        super();
+        String ll = System.getProperty("StackDumpSM.logLevel");
+        if(ll != null && ll.trim().length() > 0) {
+            logLevel = Integer.valueOf(ll);
+            log(1, " ctor, logLevel=%d", logLevel);
+        }
+        String sep = System.getProperty("StackDumpSM.traceElementSeparator");
+        if(sep != null && sep.trim().length() > 0) {
+            traceElementSeparator = sep;
+            log(1, " ctor, traceElementSeparator=%s", traceElementSeparator);
+        }
+        readProperty(includeCreateThread, false);
+        readProperty(excludeCreateThread, true);
+        readProperty(includeReadFile, false);
+        readProperty(excludeReadFile, true);
+        readProperty(includeWriteFile, false);
+        readProperty(excludeWriteFile, true);
+    }
+
+
+
+    public ThreadGroup getThreadGroup() {
+
+        dumpIfMatch(includeCreateThread, excludeCreateThread, "", "getThreadGroup");
+        return super.getThreadGroup();
+    }
+
+
+    public void checkWrite(String file) {
+        
+        dumpIfMatch(includeWriteFile, excludeWriteFile, file.replace('\\', '/'), "checkWrite");
+        super.checkWrite(file);
+    }
+  
+    public void checkRead(String file){
+
+        dumpIfMatch(includeReadFile, excludeReadFile, file.replace('\\', '/'), "checkRead");
+        super.checkWrite(file);
+    }
+
+
+    //----------------------------------------------------------------------
+
+    /**
+     * return true if param is excluded
+     */
+    private boolean matchesExcludeParam(PropertyTuple exc, String param, String prefix) {
+        if(param.length() > 0) {
+            for(Object ere : exc.values) {
+                Pattern pe = exc.regex? (Pattern)ere : null;
+                String se = (!exc.regex)? (String)ere : null;
+                if(pe != null && pe.matcher(param).matches() || se != null && param.indexOf(se) >= 0) {
+                    log(2, "checkExcludeParam %s %s matches", prefix, param);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean matchesExclude(PropertyTuple exc, String dump, String prefix) {
+        for(Object ere : exc.values) {
+            Pattern pe = exc.regex? (Pattern)ere : null;
+            String se = (!exc.regex)? (String)ere : null;
+            if(pe != null && pe.matcher(dump).matches() || se != null && dump.indexOf(se) >= 0) {
+                log(2, "dumpIfMatch %s check exclude matches %s", prefix, dump);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //----------------------------------------------------------------------
+
+    private StringBuilder dumpHeader(String prefix) {
+        return new StringBuilder("--------StackDumpSM " + prefix + " ------{{");
+    }
+
+    private StringBuilder dumpStackTraceElem(StackTraceElement ste) {
+        return new StringBuilder(ste.getClassName()).append('#').append(ste.getMethodName()).
+                    append('(').append(ste.getFileName()).append(':').append(ste.getLineNumber()).append(")").append(traceElementSeparator);
+    }
+
+    private StringBuilder dumpFooter() {
+        return new StringBuilder("--------StackDumpSecurityManager------}}");
+    }
+
+    //----------------------------------------------------------------------
+    
+    private void dumpIfMatch(PropertyTuple inc, PropertyTuple exc, String param, String prefix) {
+
+        try {
+
+        if(inc.values == null) {
+            log(2, "dumpIfMatch %s include is null. No match", prefix);
+            return;
+        }
+
+        // test if the parameter is excluded, so we don't need to do other tests
+        if(matchesExcludeParam(exc, param, prefix)) {
+            return;
+        }
+
+        Thread t = Thread.currentThread();
+        StackTraceElement[] listSTE = t.getStackTrace();
+
+        boolean matches = false;
+        StringBuilder dump = dumpHeader(prefix);
+        dump.append(param).append(traceElementSeparator);
+        for(Object ire : inc.values) {
+            Pattern pi = inc.regex? (Pattern)ire : null;
+            String si = (!inc.regex)? (String)ire : null;
+            // check if parameter matches
+            if(pi != null && pi.matcher(param).matches() || si != null && param.indexOf(si) >= 0) {
+                matches = true;
+            }
+            for(StackTraceElement ste : listSTE) {
+                StringBuilder elem = dumpStackTraceElem(ste);
+                dump.append(elem);
+
+                // check if stack elem matches
+                if(!matches) {
+                    if(pi != null && pi.matcher(new String(elem)).matches() || si != null && new String(elem).indexOf(si) >= 0) {
+                        matches = true;
+                        log(2, "dumpIfMatch %s check include matches %s", prefix, elem);
+                    }
+                }
+            }
+            if(matches) {
+                dump.append(dumpFooter());
+                break;
+            }
+        }
+        if(matches && !matchesExclude(exc, new String(dump), prefix)) {
+            System.out.println("\n\n" + dump + "\n\n");
+        }
+        } catch(Exception ex) {
+            System.err.println(ex);
+        }
+    }
+
+    //----------------------------------------------------------------------
 
     /**
      * test if system property represents a regex
@@ -49,21 +195,6 @@ public class StackDumpSecurityManager extends SecurityManager  {
             }
         }
         tuple.values = patterns;
-    }
-
-    private void logTuple(PropertyTuple tuple) {
-        if(logLevel > 0) {
-            System.out.print("----------INFO StackDumpSecurityManager ctor. " + tuple.name + " regex=" + tuple.regex + " value:");
-            if(tuple.values != null) {
-                for(Object r : tuple.values) { 
-                    System.out.print(r + ";"); 
-                } 
-                System.out.println("");
-            }
-            else {
-                System.out.println("null");
-            }
-        }
     }
 
     /**
@@ -103,141 +234,39 @@ public class StackDumpSecurityManager extends SecurityManager  {
         logTuple(tuple);  // log interpretation result
     }
 
+    //----------------------------------------------------------------------
 
-    public StackDumpSecurityManager() {
-        super();
-        String log = System.getProperty("StackDumpSM.logLevel");
-        if(log != null && log.trim().length() > 0) {
-            logLevel = Integer.valueOf(log);
-            if(logLevel > 0) { System.out.println("----------INFO StackDumpSecurityManager init. logLevel=" + logLevel); }
-        }
-        String sep = System.getProperty("StackDumpSM.traceElementSeparator");
-        if(sep != null && sep.trim().length() > 0) {
-            traceElementSeparator = sep;
-            if(logLevel > 0) { System.out.println("----------INFO StackDumpSecurityManager init. traceElementSeparator=" + traceElementSeparator); }
-        }
-        readProperty(includeCreateThread, false);
-        readProperty(excludeCreateThread, true);
-        readProperty(includeWriteFile, false);
-        readProperty(excludeWriteFile, true);
-    }
+    private static final String PREFIX = "----------";
+    private static final String LEVEL_INFO = "INFO";
+    private static final String LEVEL_DEBUG = "DEBUG";
 
-
-    /**
-     * return true if param is excluded
-     */
-    private boolean checkExcludeParam(PropertyTuple exc, String param, String prefix) {
-        if(param.length() > 0) {
-            for(Object ere : exc.values) {
-                Pattern pe = exc.regex? (Pattern)ere : null;
-                String se = (!exc.regex)? (String)ere : null;
-                if(pe != null && pe.matcher(param).matches() || se != null && param.indexOf(se) >= 0) {
-                    if(logLevel > 1) {
-                        System.out.println("----------DEBUG StackDumpSecurityManager.checkExcludeParam " + prefix + " " + param + "matches");
-                    }
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-
-    private StringBuilder dumpHeader(String prefix) {
-        return new StringBuilder("--------StackDumpSM " + prefix + " ------{{");
-    }
-
-    private StringBuilder dumpStackTraceElem(StackTraceElement ste) {
-        return new StringBuilder(ste.getClassName()).append('#').append(ste.getMethodName()).
-                    append('(').append(ste.getFileName()).append(':').append(ste.getLineNumber()).append(")").append(traceElementSeparator);
-    }
-
-    private StringBuilder dumpFooter() {
-        return new StringBuilder("--------StackDumpSecurityManager------}}");
-    }
-
-
-    private void dumpIfMatch(PropertyTuple inc, PropertyTuple exc, String param, String prefix) {
-
-        if(inc.values == null) {
-            if(logLevel > 1) {
-                System.out.println("----------DEBUG StackDump.dumpIfMatch  " + prefix + " include is null. No dump will be performed.");
-            }
+    private void log(int level, String format, Object ... args) {
+        if(level > logLevel) {
             return;
         }
 
-        // test if the parameter is excluded, so we don't need to do other tests
-        if(checkExcludeParam(exc, param, prefix)) {
-            return;
-        }
+        String prefix = String.format("%s%s StackDumpSecurityManager ", PREFIX, ((level == 1)? LEVEL_INFO : LEVEL_DEBUG));
+        String body = String.format(format, args);
+        System.out.println(prefix+body);
+    }
 
-        Thread t = Thread.currentThread();
-        StackTraceElement[] listSTE = t.getStackTrace();
-
-        boolean matches = false;
-        StringBuilder dump = dumpHeader(prefix);
-        dump.append(param).append(traceElementSeparator);
-        for(Object ire : inc.values) {
-            Pattern pi = inc.regex? (Pattern)ire : null;
-            String si = (!inc.regex)? (String)ire : null;
-            // check if parameter matches
-            if(pi != null && pi.matcher(param).matches() || si != null && param.indexOf(si) >= 0) {
-                matches = true;
+    private void logTuple(PropertyTuple tuple) {
+        if(logLevel > 0) {
+            String out = String.format("%s%s StackDumpSecurityManager logTuple %s regex=%b value=", PREFIX, LEVEL_INFO, tuple.name, tuple.regex);
+            System.out.print(out);
+            if(tuple.values != null) {
+                for(Object r : tuple.values) { 
+                    System.out.print(r + ";"); 
+                } 
+                System.out.println("");
             }
-            for(StackTraceElement ste : listSTE) {
-                StringBuilder elem = dumpStackTraceElem(ste);
-                dump.append(elem);
-
-                // check if stack elem matches
-                if(matches || pi != null && pi.matcher(new String(elem)).matches() || si != null && new String(elem).indexOf(si) >= 0) {
-                    matches = true;
-                }
-                if(logLevel > 1) {
-                    System.out.println("----------DEBUG StackDumpSecurityManager.dumpIfMatch  " + prefix + " check include" + matches);
-                }
-            }
-            if(matches) {
-                dump.append(dumpFooter());
-                break;
-            }
-        }
-        if(matches) {
-            String sdump = new String(dump);
-            for(Object ere : exc.values) {
-                Pattern pe = exc.regex? (Pattern)ere : null;
-                String se = (!exc.regex)? (String)ere : null;
-                if(pe != null && pe.matcher(sdump).matches() || se != null && sdump.indexOf(se) >= 0) {
-                    matches = false;
-                    if(logLevel > 1) {
-                        System.out.println("----------DEBUG StackDumpSecurityManager.dumpIfMatch  " + prefix + " check exclude " + (!matches));
-                    }
-                    break;
-                }
-            }
-            if(matches) {
-                System.out.println("\n\n" + dump + "\n\n");
+            else {
+                System.out.println("null");
             }
         }
     }
 
-    public ThreadGroup getThreadGroup() {
 
-        dumpIfMatch(includeCreateThread, excludeCreateThread, "", "getThreadGroup");
-        return super.getThreadGroup();
-    }
-
-
-    public void checkWrite(String file) {
-        
-        try {
-            dumpIfMatch(includeWriteFile, excludeWriteFile, file.replace('\\', '/'), "checkWrite");
-        } catch(Exception ex) {
-            System.err.println(ex);
-        }
-        
-        super.checkWrite(file);
-    }
-  
 };
 
 
